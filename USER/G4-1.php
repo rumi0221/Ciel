feabc0e4f67a7541a8455c68dccf41b75af4bcbe
Hibio117 <2201181@s.asojuku.ac.jp>
2024-10-08 13:55:09 +0900

    a

--- USER/G4-1.php
+++ USER/G4-1.php
@@ -15,28 +15,22 @@
         </nav>
     </header>
 
-    <div class="calendar">
-        <div class="month">
-            <h2>2024年10月</h2>
-        </div>
-        <table>
-            <thead>
-                <tr>
-                    <th>日</th>
-                    <th>月</th>
-                    <th>火</th>
-                    <th>水</th>
-                    <th>木</th>
-                    <th>金</th>
-                    <th>土</th>
-                </tr>
-            </thead>
-            <tbody id="firstDay">
-                <!-- カレンダーの日付が入る -->
-            </tbody>
-        </table>
-        <button class="add-btn">＋</button>
-    </div>
+    <h1 id="yearMonth"></h1>
+    <table id="calendar">
+        <thead>
+            <tr>
+                <th class="sunday">日</th>
+                <th>月</th>
+                <th>火</th>
+                <th>水</th>
+                <th>木</th>
+                <th>金</th>
+                <th class="saturday">土</th>
+            </tr>
+        </thead>
+        <tbody>
+        </tbody>
+    </table>
 
     <script src="G4-1.js"></script>
 </body>
