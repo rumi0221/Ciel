2f6012cf2c58fa647ad71bc583d8a7a4d13f994f
Hibio117 <2201181@s.asojuku.ac.jp>
2024-10-09 10:48:09 +0900

    a

--- USER/G4-1.php
+++ USER/G4-1.php
@@ -47,6 +47,42 @@
                     <td>6</td>
                     <td>7</td>
                 </tr>
+                <tr>
+                    <td>8</td>
+                    <td>9</td>
+                    <td>10</td>
+                    <td>11</td>
+                    <td>12</td>
+                    <td>13</td>
+                    <td>14</td>
+                </tr>
+                <tr>
+                    <td>15</td>
+                    <td>16</td>
+                    <td>17</td>
+                    <td>18</td>
+                    <td>19</td>
+                    <td>20</td>
+                    <td>21</td>
+                </tr>
+                <tr>
+                    <td>22</td>
+                    <td>23</td>
+                    <td>24</td>
+                    <td>25</td>
+                    <td>26</td>
+                    <td>27</td>
+                    <td>28</td>
+                </tr>
+                <tr>
+                    <td>29</td>
+                    <td>30</td>
+                    <td>31</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
                 <!-- 他の週も同様に追加 -->
             </tbody>
         </table>
