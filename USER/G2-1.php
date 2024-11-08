a96f96ce99c45e5e8709c18f3bed03402267261e
uno-2 <148303462+uno-2@users.noreply.github.com>
2024-11-08 11:10:51 +0900

    フォント変更

--- USER/G2-1.php
+++ USER/G2-1.php
@@ -51,6 +51,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/G2-1.css"/> <!-- スタイルシート -->
+    <link href="https://fonts.googleapis.com/css2?family=Inria+Serif:wght@400;700&display=swap" rel="stylesheet">
     <title>パスワード再設定</title>
 </head>
 <body>
--- USER/G2-3.php
+++ USER/G2-3.php
@@ -48,6 +48,7 @@ session_destroy();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/G2-3.css"> <!-- スタイルシート -->
+    <link href="https://fonts.googleapis.com/css2?family=Inria+Serif:wght@400;700&display=swap" rel="stylesheet">
     <title>パスワード再設定完了</title>
 </head>
 <body>
