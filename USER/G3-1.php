0bdf45fc1efaeb555aeb454ff1f9e34e267d9268
rumi0221 <148302664+rumi0221@users.noreply.github.com>
2024-11-18 12:09:15 +0900

    rename

--- USER/G3-1.php
+++ USER/G3-1.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/G3-1.css">
     <link rel="stylesheet" href="css/menu.css">
     <title>アプリ</title>
 </head>
