fa48719a9b8d78d1d87a95396d0fd7297d670a5a
Nrharuka <2201169@s.asojuku.ac.jp>
2024-11-18 12:14:06 +0900

    Update db-connect.php

--- USER/db-connect.php
+++ USER/db-connect.php
@@ -1,5 +1,5 @@
 <?php
-    const SERVER = 'mysql301.phy.lolipop.lan';
+    const SERVER = 'mysql310.phy.lolipop.lan';
     const DBNAME = 'LAA1517478-3rd';
     const USER = 'LAA1517478';
     const PASS = '3rd1004';
